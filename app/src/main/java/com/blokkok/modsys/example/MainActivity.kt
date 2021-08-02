7366d4bf512ce47804fafbc7767666deaa93078c
Iyxan23 <nurihsanalghifari@gmail.com>
2021-08-02 09:43:27 +0700

    feat: add example of invoking functions within the host

--- app/src/main/java/com/blokkok/modsys/example/MainActivity.kt
+++ app/src/main/java/com/blokkok/modsys/example/MainActivity.kt
@@ -2,9 +2,11 @@ package com.blokkok.modsys.example
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.blokkok.modsys.ModuleManager
 import com.blokkok.modsys.example.databinding.ActivityMainBinding
+import com.blokkok.modsys.modinter.exception.NotDefinedException
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,8 +26,19 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
-        ModuleManager.registerCommunications {
-            // TODO: This
+        binding.callFunc.setOnClickListener {
+            try {
+                ModuleManager.executeCommunications {
+                    invokeFunction("/example-module", "say-hello")
+                    invokeFunction("/example-module", "say-something", listOf("Something"))
+                }
+            } catch (e: NotDefinedException) {
+                Toast.makeText(
+                    this,
+                    "The module isn't loaded",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
